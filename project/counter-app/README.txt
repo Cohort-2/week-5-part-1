6040ee9f2b88804c7aa95204da7d677dafe98aa9
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-10-02 13:21:01 +0530

    counter app

---
+++ project/counter-app/README.txt
@@ -0,0 +1,12 @@
+how to create a react app with vite ?
+
+index.html --> main.jsx --> App.jsx
+
+lets create a counter app using react 
+// whenever we need to use javascript within react we use curly braces for more check app.jsx
+
+step 1 : create a react app
+step 2 : created a button 
+step 3 : defined a state using useState 
+step 4 : wrote a function to update the state 
+step 5 : added the function to the button
---
+++ project/counter-app/src/App.jsx
@@ -0,0 +1,35 @@
+//state and component
+
+import { useState } from "react";
+
+// let state = {
+//   count: 0, // this is my state
+// }; // according to react this is not a state variable you cannot import anything and say its state you need to do it in a certain way that react watches it and updates the state
+
+// now lets define state a different way
+
+//jsx
+// in html its like this
+// <button onClick="onButtonPress()">Counter {state.count}</button>
+// in jsx its like this
+// <button onClick={onButtonPress}>Counter {state.count}</button>
+// now i want on click of the button the state should update
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  function onButtonPress() {
+    // state.count++; // till now nothing happens
+    // console.log(state.count);
+    // count = count + 1; // this is not the right way to update state
+    setCount(count + 1); // this is the right way to update state
+  }
+
+  return (
+    <div>
+      <button onClick={onButtonPress}>Counter {count}</button>
+    </div>
+  );
+};
+
+export default App;
